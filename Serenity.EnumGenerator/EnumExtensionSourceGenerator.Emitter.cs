11c85a01209dad38f2c0b0a7113da69bb9450790
tahacorbacioglu <tahacorbacioglu@gmail.com>
2024-01-26 18:44:48 +0300

    Change source generator to incremental

---
+++ Serenity.EnumGenerator/EnumExtensionSourceGenerator.Emitter.cs
@@ -0,0 +1,138 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Serenity.EnumGenerator;
+
+public partial class EnumExtensionSourceGenerator
+{
+    internal class Emitter
+    {
+        private readonly SourceProductionContext _context;
+        private readonly ParseResult[] _results;
+        private readonly CodeWriter _codeWriter;
+
+        public Emitter(SourceProductionContext context, ParseResult[] results)
+        {
+            _context = context;
+            _results = results;
+            _codeWriter = new CodeWriter();
+        }
+
+        public void Emit()
+        {
+            foreach (var result in _results)
+            {
+                _codeWriter.Clear();
+
+                var sourceText = WriteExtensions(result);
+                _context.AddSource($"{result.Name}Extensions.g.cs", SourceText.From(sourceText, Encoding.UTF8));
+            }
+        }
+
+
+        private string WriteExtensions(ParseResult result)
+        {
+            _codeWriter.AppendLine("using System;");
+            _codeWriter.AppendLine();
+
+            if (!string.IsNullOrEmpty(result.Namespace))
+            {
+                _codeWriter.AppendLine($"namespace {result.Namespace}");
+                _codeWriter.BeginBlock();
+            }
+
+            _codeWriter.AppendLine($"public static partial class {result.Name}Extensions");
+            _codeWriter.BeginBlock();
+
+            AppendLength(result);
+            _codeWriter.AppendLine();
+            AppendToStringFast(result);
+            _codeWriter.AppendLine();
+            AppendValuesFast(result);
+            _codeWriter.AppendLine();
+            AppendNamesFast(result);
+            _codeWriter.AppendLine();
+            AppendIsDefinedFast(result);
+
+            if (result.HasFlagAttribute)
+            {
+                _codeWriter.AppendLine();
+                AppendHasFlagFast(result.Name);
+            }
+
+            if (!string.IsNullOrEmpty(result.Namespace)) _codeWriter.EndBlock();
+
+            _codeWriter.EndBlock();
+            return _codeWriter.ToString();
+        }
+
+        private void AppendLength(ParseResult e)
+        {
+            _codeWriter.AppendLine(
+                $"public const int Length = {e.EnumMembers.Count().ToString()};");
+        }
+
+        private void AppendToStringFast(ParseResult result)
+        {
+            using (_codeWriter.BeginBlockScope(
+                       $"public static string ToStringFast(this {result.Name} value)"))
+            {
+                using (_codeWriter.BeginBlockScope("return value switch", true))
+                {
+                    foreach (var member in result.EnumMembers)
+                        _codeWriter.AppendLine($"{result.Name}.{member} => nameof({result.Name}.{member}),");
+
+                    _codeWriter.AppendLine("_ => value.ToString()");
+                }
+            }
+        }
+
+        private void AppendValuesFast(ParseResult result)
+        {
+            using (_codeWriter.BeginBlockScope($"public static {result.Name}[] GetValuesFast()"))
+            {
+                using (_codeWriter.BeginBlockScope("return new[]", true))
+                {
+                    foreach (var member in result.EnumMembers)
+                        _codeWriter.AppendLine($"{result.Name}.{member},");
+                }
+            }
+        }
+
+        private void AppendNamesFast(ParseResult result)
+        {
+            using (_codeWriter.BeginBlockScope($"public static string[] GetNamesFast()"))
+            {
+                using (_codeWriter.BeginBlockScope("return new[]", true))
+                {
+                    foreach (var member in result.EnumMembers)
+                        _codeWriter.AppendLine($@"nameof({result.Name}.{member}),");
+                }
+            }
+        }
+
+        private void AppendIsDefinedFast(ParseResult result)
+        {
+            using (_codeWriter.BeginBlockScope($"public static bool IsDefinedFast({result.Name} value)"))
+            {
+                using (_codeWriter.BeginBlockScope("return value switch", true))
+                {
+                    foreach (var member in result.EnumMembers)
+                        _codeWriter.AppendLine($@"{result.Name}.{member} => true,");
+
+                    _codeWriter.AppendLine("_ => false");
+                }
+            }
+        }
+
+        private void AppendHasFlagFast(string enumName)
+        {
+            using (_codeWriter.BeginBlockScope(
+                       $"public static bool HasFlagFast(this {enumName} value, {enumName} flag)"))
+            {
+                _codeWriter.AppendLine($"return flag == 0 ? true : (value & flag) == flag;");
+            }
+        }
+    }
+}
--- Serenity.EnumGenerator/EnumExtensionSourceGenerator.cs
+++ Serenity.EnumGenerator/EnumExtensionSourceGenerator.cs
@@ -1,175 +1,45 @@
-﻿using System.Text;
+﻿using System.Collections.Immutable;
+using System.Text;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Serenity.EnumGenerator;
 
 [Generator(LanguageNames.CSharp)]
-public class EnumExtensionSourceGenerator : ISourceGenerator
+public partial class EnumExtensionSourceGenerator : IIncrementalGenerator
 {
-    public const string NameSpace = "Serenity.EnumGenerator";
-    public const string AttributeName = "EnumExtensions";
-
-    private const string ExtensionMethodNameGetLength = "Length";
-    private const string ExtensionMethodNameToString = "ToStringFast";
-    private const string ExtensionMethodNameGetValues = "GetValuesFast";
-    private const string ExtensionMethodNameGetNames = "GetNamesFast";
-    private const string ExtensionMethodNameIsDefined = "IsDefinedFast";
-    private const string ExtensionMethodNameHasFlag = "HasFlagFast";
-
-    private const string Attribute = $@"
-using System;
-
-namespace {NameSpace}
-{{
-    [AttributeUsage(AttributeTargets.Enum)]
-    public sealed class {AttributeName}Attribute : Attribute
-    {{
-    }}
-}}";
-
-    public void Initialize(GeneratorInitializationContext context)
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        var source = context.SyntaxProvider.ForAttributeWithMetadataName(
+            Attributes.EnumExtensionsDisplayName,
+            static (node, _) => node is EnumDeclarationSyntax,
+            static (context, _) => context);
+
+        context.RegisterPostInitializationOutput((postInitializationContext) =>
+        {
+            postInitializationContext.AddSource($"{Attributes.EnumExtensionsName}.g.cs",
+                SourceText.From(Attributes.EnumExtensionsAttribute, Encoding.UTF8));
+        });
+        context.RegisterSourceOutput(context.CompilationProvider.Combine(source.Collect()), Execute);
     }
 
-    public void Execute(GeneratorExecutionContext context)
+    private static void Execute(SourceProductionContext context,
+        (Compilation, ImmutableArray<GeneratorAttributeSyntaxContext>) tuple)
     {
-        var moduleName = context.Compilation.SourceModule.Name.AsSpan();
-        if (moduleName.StartsWith("UnityEngine.")) return;
-        if (moduleName.StartsWith("UnityEditor.")) return;
-        if (moduleName.StartsWith("Unity.")) return;
+        var (compilation, sources) = tuple;
+        if (sources.Length == 0) return;
 
-        context.AddSource($"{AttributeName}Attribute.g.cs", SourceText.From(Attribute, Encoding.UTF8));
+        var moduleName = compilation.SourceModule.Name;
+        if (moduleName.StartsWith("UnityEngine.") ||
+            moduleName.StartsWith("UnityEditor.") ||
+            moduleName.StartsWith("Unity.")) return;
 
-        var syntaxReceiver = (SyntaxReceiver)context.SyntaxReceiver!;
-
-        if (syntaxReceiver.WorkItems.Count == 0) return;
-
-        var codeWriter = new CodeWriter();
-        foreach (var workItem in syntaxReceiver.WorkItems)
+        var result = new Parser(sources).Parse();
+        if (result.Length != 0)
         {
-            var semanticModel = context.Compilation.GetSemanticModel(workItem.EnumDeclarationSyntax.SyntaxTree);
-            if (semanticModel.GetDeclaredSymbol(workItem.EnumDeclarationSyntax) is not INamedTypeSymbol enumSymbol)
-                continue;
-
-            var symbol = semanticModel.GetDeclaredSymbol(workItem.EnumDeclarationSyntax);
-
-            if (symbol == null) continue;
-
-            var sourceText = WriteExtensions(codeWriter,
-                symbol.ContainingNamespace.IsGlobalNamespace ? string.Empty : symbol.ContainingNamespace.ToString(),
-                symbol.Name, workItem);
-            context.AddSource($"{symbol.Name}Extensions.g.cs", SourceText.From(sourceText, Encoding.UTF8));
-            codeWriter.Clear();
-        }
-    }
-
-    private static string WriteExtensions(in CodeWriter codeWriter, string namespaceName, string enumName,
-        WorkItem workItem)
-    {
-        codeWriter.AppendLine("using System;");
-        codeWriter.AppendLine();
-
-        if (!string.IsNullOrEmpty(namespaceName))
-        {
-            codeWriter.AppendLine($"namespace {namespaceName}");
-            codeWriter.BeginBlock();
-        }
-
-        codeWriter.AppendLine($"public static partial class {enumName}Extensions");
-        codeWriter.BeginBlock();
-
-        AppendLength(codeWriter, workItem);
-        codeWriter.AppendLine();
-        AppendToStringFast(codeWriter, enumName, workItem);
-        codeWriter.AppendLine();
-        AppendValuesFast(codeWriter, enumName, workItem);
-        codeWriter.AppendLine();
-        AppendNamesFast(codeWriter, enumName, workItem);
-        codeWriter.AppendLine();
-        AppendIsDefinedFast(codeWriter, enumName, workItem);
-
-        if (workItem.HasFlagAttribute)
-        {
-            codeWriter.AppendLine();
-            AppendHasFlagFast(codeWriter, enumName);
-        }
-
-        if (!string.IsNullOrEmpty(namespaceName))
-        {
-            codeWriter.EndBlock();
-        }
-
-        codeWriter.EndBlock();
-        return codeWriter.ToString();
-    }
-
-    private static void AppendLength(in CodeWriter sourceBuilder, WorkItem e)
-    {
-        sourceBuilder.AppendLine(
-            $"public const int {ExtensionMethodNameGetLength} = {e.EnumMembers.Count().ToString()};");
-    }
-
-    private static void AppendToStringFast(in CodeWriter sourceBuilder, string enumName, WorkItem workItem)
-    {
-        using (sourceBuilder.BeginBlockScope(
-                   $"public static string {ExtensionMethodNameToString}(this {enumName} value)"))
-        {
-            using (sourceBuilder.BeginBlockScope("return value switch", true))
-            {
-                foreach (var member in workItem.EnumMembers)
-                    sourceBuilder.AppendLine($"{enumName}.{member} => nameof({enumName}.{member}),");
-
-                sourceBuilder.AppendLine("_ => value.ToString()");
-            }
-        }
-    }
-
-    private static void AppendValuesFast(in CodeWriter sourceBuilder, string enumName, WorkItem workItem)
-    {
-        using (sourceBuilder.BeginBlockScope($"public static {enumName}[] {ExtensionMethodNameGetValues}()"))
-        {
-            using (sourceBuilder.BeginBlockScope("return new[]", true))
-            {
-                foreach (var member in workItem.EnumMembers)
-                    sourceBuilder.AppendLine($"{enumName}.{member},");
-            }
-        }
-    }
-
-    private static void AppendNamesFast(in CodeWriter sourceBuilder, string enumName, WorkItem workItem)
-    {
-        using (sourceBuilder.BeginBlockScope($"public static string[] {ExtensionMethodNameGetNames}()"))
-        {
-            using (sourceBuilder.BeginBlockScope("return new[]", true))
-            {
-                foreach (var member in workItem.EnumMembers)
-                    sourceBuilder.AppendLine($@"nameof({enumName}.{member}),");
-            }
-        }
-    }
-
-    private static void AppendIsDefinedFast(in CodeWriter sourceBuilder, string enumName, WorkItem workItem)
-    {
-        using (sourceBuilder.BeginBlockScope($"public static bool {ExtensionMethodNameIsDefined}({enumName} value)"))
-        {
-            using (sourceBuilder.BeginBlockScope("return value switch", true))
-            {
-                foreach (var member in workItem.EnumMembers)
-                    sourceBuilder.AppendLine($@"{enumName}.{member} => true,");
-
-                sourceBuilder.AppendLine("_ => false");
-            }
-        }
-    }
-
-    private static void AppendHasFlagFast(in CodeWriter sourceBuilder, string enumName)
-    {
-        using (sourceBuilder.BeginBlockScope(
-                   $"public static bool {ExtensionMethodNameHasFlag}(this {enumName} value, {enumName} flag)"))
-        {
-            sourceBuilder.AppendLine($"return flag == 0 ? true : (value & flag) == flag;");
+            var emitter = new Emitter(context, result);
+            emitter.Emit();
         }
     }
 }
